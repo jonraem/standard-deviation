67cf69658173d2dec58528e071bc5b2f823013b4
Joni Rämö <joni.ramo@nordsoftware.com>
2017-04-04 22:46:36 +0300

    Add source and README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# standard-deviation
+Functions for standard deviation operations.
+* Written with ES6.
+* No dependencies required.
+* Inspired by [funfunfunction](https://www.youtube.com/watch?v=9-8E8L_77-8).
---
+++ standard-deviation.js
@@ -0,0 +1,14 @@
+// average of an array
+export const arrayAverage = (arr) => arr.reduce((acc, val) => acc + val, 0) / arr.length;
+
+// differences of array values
+export const differences = (arr) => arr.map(x => x - arrayAverage(arr)).map(x => x * x);
+
+// i.e. average difference
+export const variance = (arr) => arrayAverage(differences(arr));
+
+// standard deviation of array values
+export const standardDeviation = (arr) => Math.sqrt(variance(arr));
+
+// outlier values in array
+export const findOutliers = (arr) => arr.map(x => x - arrayAverage(arr) - standardDeviation(arr) > 0);
